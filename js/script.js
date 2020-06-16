03f48f7a7629b97e4637f4084452eda67c2343cd
ubitinonevami <bernastya@inbox.ru>
2020-06-16 11:54:21 +0400

    aa

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var link = document.querySelector('.address-button');
 var closeButton = document.querySelector('.popup-close-button');
 var login = popup.querySelector('[name="person-name"]');
 var email = popup.querySelector('[name="person-email"]');
+var textarea = popup.querySelector('[name=textarea]');
 var form = popup.querySelector('.popup-form');
 var storage = localStorage.getItem('name');
 
@@ -27,7 +28,7 @@ closeButton.addEventListener('click', function(evt) {
 });
 
 form.addEventListener('submit', function(evt) {
-	if (!login.value || !email.value) {
+	if (!login.value || !email.value || !textarea.value) {
 		evt.preventDefault();
 		popup.classList.remove('modal-error');
 		popup.offsetWidth = popup.offsetWidth;
